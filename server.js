9bc6d0521bc9b98a5f51b19e7d649936463edb26
praveensvk1408 <104765721+praveensvk1408@users.noreply.github.com>
2024-03-04 11:30:40 +0530

    close 1

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ const homePath = "/home/ubuntu/.jenkins/workspace/"
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "public")));
 // sendFile will go here
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render(path.join(__dirname, 'public/index.html'));
 });
 
-app.get("/data/:name",(req,res)=>{
-  if(fs.existsSync(homePath + req.params.name + "/microservices/webbff/test/e2e/goReportJSONFile/getProduct_output.json")){
-    console.log("exists")
-    const jsonString = fs.readFileSync(homePath + req.params.name + "/microservices/webbff/test/e2e/goReportJSONFile/getProduct_output.json") 
+app.get("/data/:name", (req, res) => {
+  if (fs.existsSync(homePath + req.params.name + "/microservices/webbff/test/e2e/goReportJSONFile/getProduct_output.json")) {
+    console.log("exs")
+    const jsonString = fs.readFileSync(homePath + req.params.name + "/microservices/webbff/test/e2e/goReportJSONFile/getProduct_output.json")
     const data = JSON.parse(jsonString)
-    res.json(data)  
-  }else{
+    res.json(data)
+  } else {
     console.log("does not exist")
-    res.json({successNum:0,failNum:0})
+    res.json({ successNum: 0, failNum: 0 })
   }
 }
 )
